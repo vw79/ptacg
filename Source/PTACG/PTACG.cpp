5e12f7e600030138df215668b8744ee0ff487f03
vw79 <wshong8282@gmail.com>
2024-09-08 18:03:13 +0800

    multiplayer setup foundation

---
+++ Source/PTACG/PTACG.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PTACG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PTACG, "PTACG" );
